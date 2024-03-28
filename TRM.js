76c3001add6d9b6862f42e0a0537ea9816dc4131
samanthahealius <162088099+samanthahealius@users.noreply.github.com>
2024-03-28 16:28:24 +1100

    Update TRM.js

--- TRM.js
+++ TRM.js
@@ -56,7 +56,7 @@ if (domain.includes('laverty')) {
     sCompanyAbbr = 'WDP';
 }
 else{
-    sCompanyAbbr = 'LAV';
+    sCompanyAbbr = 'TML';
 }
 
 console.log(sCompanyAbbr); // For testing purposes, to see the output
@@ -670,6 +670,7 @@ Manual.prototype.open = function(){
 		// 		}
 		// 	});
 		// });
+		
 	}
 	this.opened = true;
 	this.title.removeClass('ui-corner-all').addClass('ui-accordion-header-active ui-state-active ui-corner-top');
@@ -687,3 +688,43 @@ Manual.prototype.close = function(){
 	if(this.content !== null) this.content.removeClass('ui-accordion-content-active').hide();
 	return this.opened;
 };
+
+document.addEventListener('DOMContentLoaded', function() {
+    document.body.addEventListener('click', function(e) {
+        // Hide any existing tooltips
+        var existingTooltip = document.querySelector('.tooltip-content');
+        if (existingTooltip) {
+            existingTooltip.parentNode.removeChild(existingTooltip);
+        }
+
+        // Check if the clicked element is supposed to show a tooltip
+        if (e.target.classList.contains('pseudo')) {
+            e.preventDefault(); // Prevent the default link action
+
+            var tooltipContent = e.target.getAttribute('data-tooltip');
+            var tooltipDiv = document.createElement('div');
+            tooltipDiv.classList.add('tooltip-content');
+            tooltipDiv.innerHTML = tooltipContent;
+            
+            // Position the tooltip
+            var rect = e.target.getBoundingClientRect();
+            tooltipDiv.style.top = (window.scrollY + rect.top - rect.height) + 'px'; // Adjust this logic as needed
+            tooltipDiv.style.left = (rect.left + window.scrollX) + 'px'; // Adjust this logic as needed
+
+            document.body.appendChild(tooltipDiv);
+            
+            // Show the tooltip
+            tooltipDiv.style.display = 'block';
+        }
+    }, false);
+
+    // Optional: Hide tooltip when clicking anywhere else on the page
+    document.addEventListener('click', function(e) {
+        if (!e.target.classList.contains('pseudo')) {
+            var existingTooltip = document.querySelector('.tooltip-content');
+            if (existingTooltip) {
+                existingTooltip.style.display = 'none';
+            }
+        }
+    });
+});
